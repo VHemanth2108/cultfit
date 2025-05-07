5725d975f17c29f5a5bf320bb7db59ee77f8cae1
VHemanth2108 <hemanthvasireddi2@gmail.com>
2025-05-07 12:42:53 +0530

    Set base path for GitHub Pages

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/cultfit/', // 👈 Add this line
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+})
